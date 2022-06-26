97795edd7340ef5e876cff97b6e0af3757acb185
LeoXutianzeXu <leoxutianzexu@gmail.com>
2022-06-27 06:12:45 +0800

    PsychEngine Update

--- Elle Mod/mods/Pencil Create.lua
+++ Elle Mod/mods/Pencil Create.lua
@@ -3,23 +3,23 @@ function onCreate()
 	-- triggered when the lua file is started
 
 	-- create a lua sprite
-	makeAnimatedLuaSprite('sexualintercourse', 'characters/DADDY_DEAREST', -100, 100);
-	addAnimationByPrefix('sexualintercourse', 'first', 'Dad idle dance', 24, false);
-	objectPlayAnimation('sexualintercourse', 'first');
-	addLuaSprite('sexualintercourse', false); -- false = add behind characters, true = add over characters
+	makeAnimatedLuaSprite('pencil', 'characters/DADDY_DEAREST', -100, 100);
+	addAnimationByPrefix('pencil', 'first', 'Dad idle dance', 24, false);
+	objectPlayAnimation('pencil', 'first');
+	addLuaSprite('pencil', false); -- false = add behind characters, true = add over characters
 end
 
 -- Gameplay interactions
 function onBeatHit()
 	-- triggered 4 times per section
 	if curBeat % 2 == 0 then
-		objectPlayAnimation('sexualintercourse', 'first');
+		objectPlayAnimation('pencil', 'first');
 	end
 end
 
 function onStepHit()
 	-- triggered 16 times per section
-	setProperty('sexualintercourse.scale.x', getProperty('sexualintercourse.scale.x') + 0.01);
+	setProperty('pencil.scale.x', getProperty('pencil.scale.x') + 0.01);
 end
 
 function onCountdownTick(counter)
@@ -29,6 +29,6 @@ function onCountdownTick(counter)
 	-- counter = 3 -> "Go!"
 	-- counter = 4 -> Nothing happens lol, tho it is triggered at the same time as onSongStart i think
 	if counter % 2 == 0 then
-		objectPlayAnimation('sexualintercourse', 'first');
+		objectPlayAnimation('pencil', 'first');
 	end
 end
--- Elle Mod/mods/custom_events/Pencil Draw.lua
+++ Elle Mod/mods/custom_events/Pencil Draw.lua
@@ -1,28 +1,64 @@
 -- Event notes hooks
 function onEvent(name, value1, value2)
 	if name == 'Pencil Draw' then
+
 		duration = tonumber(value1);
 		enter_duration = tonumber(value2);
+		x_position = tonumber(value3);
+		y_position = tonumber(value4);
 		if duration < 0 then
 			duration = 0;
 		end
 		if enter_duration < 0 then
 			enter_duration = 0;
 		end
+		
 
+		makeAnimatedLuaSprite('pencil', 'characters/PencilDraw', -100, 100);
+		addAnimationByPrefix('pencil', 'first', 'BF NOTE DOWN', 24, false);
+		objectPlayAnimation('pencil', 'first');
+		addLuaSprite('pencil', false); -- false = add behind characters, true = add over characters
+		
+	
+		makeAnimatedLuaSprite('pencil', 'characters/bfPixelsDEAD', -100, 100);
+		addAnimationByPrefix('pencil', 'first', 'BF Dies pixel', 24, false);
+		objectPlayAnimation('pencil', 'first');
+		addLuaSprite('pencil', false); -- false = add behind characters, true = add over characters
+
+		makeAnimatedLuaSprite('pencil', 'characters/momCar', -100, 100);
+		addAnimationByPrefix('pencil', 'first', 'MOM DOWN POSE', 24, false);
+		objectPlayAnimation('pencil', 'first');
+		addLuaSprite('pencil', false); -- false = add behind characters, true = add over characters
 
 
 		--[[
-		targetAlpha = tonumber(value2);
-		if duration == 0 then
-			setProperty('dad.alpha', targetAlpha);
-			setProperty('iconP2.alpha', targetAlpha);
-		else
-			doTweenAlpha('dadFadeEventTween', 'dad', targetAlpha, duration, 'linear');
-			doTweenAlpha('iconDadFadeEventTween', 'iconP2', targetAlpha, duration, 'linear');
-		end
+		makeAnimatedLuaSprite('pencil', 'characters/Pencil Draw', 500, 0);
+		addAnimationByPrefix('pencil', 'first', 'pencil', 24, false);
+		objectPlayAnimation('pencil', 'first');
+		addLuaSprite('pencil', false); -- false = add behind characters, true = add over characters
 		]]--
+	end
+end
 
-		debugPrint('Event triggered: ', name, duration, enter_duration);
+function onStepHit()
+	-- triggered 16 times per section
+	setProperty('pencil.scale.x', getProperty('pencil.scale.x') + 0.01);
+end
+
+function onBeatHit()
+	-- triggered 4 times per section
+	if curBeat % 2 == 0 then
+		objectPlayAnimation('pencil', 'first');
+	end
+end
+
+function onCountdownTick(counter)
+	-- counter = 0 -> "Three"
+	-- counter = 1 -> "Two"
+	-- counter = 2 -> "One"
+	-- counter = 3 -> "Go!"
+	-- counter = 4 -> Nothing happens lol, tho it is triggered at the same time as onSongStart i think
+	if counter % 2 == 0 then
+		objectPlayAnimation('pencil', 'first');
 	end
 end
